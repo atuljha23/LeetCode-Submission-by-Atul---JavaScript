96b08998ef7111520bc9af8900d431dc6d91a775
Atul Jha <atul23.jha@gmail.com>
2022-04-24 23:46:42 +0100

    Time: 86 ms, Memory: 44.2 MB - LeetHub

---
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,9 @@
+var findNumbers = function(nums) {
+    let count = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i].toString().length % 2 === 0) {
+            count++
+        }
+    }
+    return count;
+};
