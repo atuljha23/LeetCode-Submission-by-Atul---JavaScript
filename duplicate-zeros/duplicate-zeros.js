7e5a8df29276c7cd34c752fb0367efb62e4459b6
Atul Jha <atul23.jha@gmail.com>
2022-04-25 00:19:22 +0100

    Time: 80 ms, Memory: 44.2 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    let length = arr.length;
+    for(let i=0; i<length;i++){
+        if(arr[i] === 0) {
+            arr.splice(i,0,0)
+            arr.splice(arr.length-1,1)
+            i++
+        }
+    }
+    return arr;
+};
