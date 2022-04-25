b3c6a26629e86a1ccb187a042b2f6f1905d82027
Atul Jha <atul23.jha@gmail.com>
2022-04-25 22:04:18 +0100

    Time: 77 ms, Memory: 42.2 MB - LeetHub

--- remove-element/remove-element.js
+++ remove-element/remove-element.js
@@ -7,7 +7,7 @@ var removeElement = function(nums, val) {
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] == val) {
             nums.splice(i, 1);
-            i -= 1
+            i--
         }
     }
 }
