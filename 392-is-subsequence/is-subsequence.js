f30718f6ef12d8225a7a69c9928748af21386141
Atul Jha <atul23.jha@gmail.com>
2025-04-01 16:15:32 +0100

    Time: 0 ms (100.00%) | Memory: 54.3 MB (57.43%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let j = 0;
+    for(let i=0; i < t.length; i++) {
+        if(t[i] === s[j]) {
+            j++
+        }
+    } if(j === s.length) return true 
+    else return false
+};
